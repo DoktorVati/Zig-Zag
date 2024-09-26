c28817cdb7152adb5dde773c5a2eb56f016f661b
Noah Sylvester <noahsylvester594@gmail.com>
2024-09-26 12:49:01 -0400

    this includes the base version of android zigzag

---
+++ Android/ZigZag/app/src/main/java/com/zigzag/MainActivity.java
@@ -0,0 +1,35 @@
+package com.zigzag;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declare variables for the TextView and Button
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Link to activity_main.xml layout
+
+        // Initialize the TextView and Button by finding them in the layout
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        // Set a click listener for the button to change the text in TextView
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Button Clicked!");
+            }
+        });
+    }
+}
