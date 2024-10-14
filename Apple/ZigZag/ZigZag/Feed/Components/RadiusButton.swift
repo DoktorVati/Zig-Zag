9e638ec2cb5226e9057299bf1398ecdf90b20ffa
Daniel <dwinsness@gmail.com>
2024-10-14 16:17:10 -0400

    Create RadiusButton to be able to change map radius

--- Apple/ZigZag/ZigZag/Feed/Components/RadiusButton.swift
+++ Apple/ZigZag/ZigZag/Feed/Components/RadiusButton.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct RadiusButton: View {
+    @EnvironmentObject var viewModel: FeedViewModel
+    
+    var icon: String
+    var myIndex: Int
+    var action: () -> Void
+    
+    var isPressed: Bool {
+        if viewModel.selectedRadiusIndex == myIndex {
+            return true
+        } else {
+            return false
+        }
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            action()
+        } label: {
+            Image(systemName: icon)
+                .foregroundStyle(isPressed ? Color(.white) : Color(UIColor.label))
+                .padding()
+//                .background(
+//                    RoundedRectangle(cornerRadius: CGFloat(20))
+//                        .foregroundStyle(isPressed ? Color.blue : Color(UIColor.systemBackground))
+//                )
+        }
+
     }
 }
 
 #Preview {
-    RadiusButton()
+    RadiusButton(icon: "globe.americas", myIndex: 0) { }
+        .environmentObject(FeedViewModel())
 }
